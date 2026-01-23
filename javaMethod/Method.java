80d6f46112b539c6ea7e0f2f9e18306ecfe14d70
Kabir202005 <Kabir202005@gmail.com>
2026-01-23 21:45:35 +0530

    passing multiple parameters to the method

--- javaMethod/Method.java
+++ javaMethod/Method.java
@@ -12,3 +12,5 @@ public class Method {
         myMethod("45");
     }
 }
+
+
---
+++ javaMethod/Method2.java
@@ -0,0 +1,11 @@
+public class Method2 {
+    static void myMethod(String name, int age){
+        System.out.println("Name: " + name + ",Age:" +age);
+    }
+    public static void main(String[] srgs){
+        myMethod("Kartik", 21);
+        myMethod("Rohan", 22);
+        myMethod("Shalini" , 20);
+    }
+    
+}
