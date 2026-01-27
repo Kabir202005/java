998acce78e2331dda56618000fba1f2c6617416e
Kabir202005 <Kabir202005@gmail.com>
2026-01-27 23:01:41 +0530

    Java Scope

---
+++ javaMethod/javaScope.java
@@ -0,0 +1,9 @@
+// Scope are known as the variables are only accesible inside the region where they are defined.
+public class javaScope {
+    public static void main(String[] args){
+        // Here code cannot use x
+        int x = 3;
+        // Here code can use x
+        System.out.println(x);
+    }
+}
