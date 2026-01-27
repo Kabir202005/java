57afecc4c28847942482c63243990a7b85cc02c9
Kabir202005 <Kabir202005@gmail.com>
2026-01-27 23:07:21 +0530

    Java Scope

--- javaMethod/javaScope.java
+++ javaMethod/javaScope.java
@@ -7,3 +7,48 @@ public class javaScope {
         System.out.println(x);
     }
 }
+// In Java Scope we also have Block Scope and Loop Scope
+
+// BLOCK SCOPE
+
+// - A block of code refers to the code between the curly braces{}.
+// - Variables declared inside a block of code are only accessible within that block.
+
+
+/*  public class javaScope {
+  public static void main(String[] args) {
+
+    // Code here CANNOT use x
+
+    { // This is a block
+
+      // Code here CANNOT use x
+
+      int x = 100;
+
+      // Code here CAN use x
+      System.out.println(x);
+
+    } // The block ends here
+
+    // Code here CANNOT use x
+
+  }
+}
+  */
+
+
+// LOOP SCOPE
+
+// - variables declared inside a loop are only accessible within that loop.
+
+/*public class Main {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i); // i is accessible here
+    }
+
+    // i is NOT accessible here
+  }
+} */
