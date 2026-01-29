a7ff617e4461a8e130620daa4004126be82bca97
Kabir202005 <Kabir202005@gmail.com>
2026-01-29 23:12:19 +0530

    factorial function with recursion

--- javaMethod/Fact.java
+++ javaMethod/Fact.java
@@ -1,3 +1,4 @@
+// Factorial means multiplying a number by every number below it, down to 1. For example, the factorial of 5 is: 5 * 4 * 3 * 2 * 1 = 120. By definition, 0! is also 1.
 public class Fact {
       public static int factorial(int n) {
     if (n > 1) {
