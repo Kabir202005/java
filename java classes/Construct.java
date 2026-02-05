d8e473620d6dda93a8ff7b0d4f7a04399549c04c
Kabir202005 <Kabir202005@gmail.com>
2026-02-05 23:25:25 +0530

    Java Constructor

---
+++ java classes/Construct.java
@@ -0,0 +1,10 @@
+public class Construct {
+    int x;
+    public Construct(){
+        x=5;
+    }
+    public static void main(String[] args){
+        Construct myobj = new Construct();
+        System.out.println(myobj.x);
+    }
+}
