03c6217118f748c851be20a458348dfa0adb8eb4
Kabir202005 <Kabir202005@gmail.com>
2026-02-10 00:49:25 +0530

    Constructor with parameter

---
+++ java classes/conspara.java
@@ -0,0 +1,10 @@
+public class conspara {
+    int x;
+    public conspara(int y){
+        x = y;
+    }
+    public static void main(String[] args){
+        conspara myObj = new conspara(5);
+        System.out.println(myObj.x);
+    }
+}
