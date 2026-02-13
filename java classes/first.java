a9791267d63c668dc6623dcaf6a78126eab8ad75
Kabir202005 <Kabir202005@gmail.com>
2026-02-14 00:49:26 +0530

    initial commit

--- java classes/first.java
+++ java classes/first.java
@@ -1,20 +1,12 @@
-// // In this we learn that how multiple classes can be defined in a single file, create an object of a class and access it in another class.
-// public class first {
-//     public void fullThrottle(){
-//         System.out.println("The car is going as fast as it can");
-//     }
-
-//     public void speed(int maxspeed){
-//         System.out.println("Max speed is: "+ maxspeed);
-//     }
-    
-// }
-
-public class first{
-    public static void main(String[] args){
-        int[] marks ={"volvo", " BMW", "Audi", "Honda"};
-        for(int i=0;i<cars.length;i++){
-            System.out.println(cars[i]);
-        } 
+// In this we learn that how multiple classes can be defined in a single file, create an object of a class and access it in another class.
+public class first {
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can");
     }
+
+    public void speed(int maxspeed){
+        System.out.println("Max speed is: "+ maxspeed);
+    }
+    
 }
+
