07f41e9ec9053842f75ca79c27be069af2f8b8f6
Kabir202005 <Kabir202005@gmail.com>
2026-02-14 23:15:26 +0530

    Question 1

---
+++ Question/person.java
@@ -0,0 +1,14 @@
+// Inside Person, complete the following steps:
+// Create a String variable named name and give it a valid name
+// Create an int variable named age and give it a valid value
+// Inside main(), create an object of Person
+// Print name and age using the object (you can print just the value, or add text before it)
+public class person {
+   String name = "Kabir";
+   int age = 25;
+    public static void main(String[] args){
+        person p1 = new person();
+        System.out.println(p1.name);
+        System.out.println(p1.age);
+    }
+}
