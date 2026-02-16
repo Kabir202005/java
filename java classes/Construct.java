19c0d08ab1c5435c36b470f95f0c5908c936a07d
Kabir202005 <Kabir202005@gmail.com>
2026-02-16 20:56:35 +0530

    Initial commit

--- java classes/Construct.java
+++ java classes/Construct.java
@@ -1,7 +1,10 @@
-public class Construct {
+// Constructor in java is a special method that is used to initialize the object,
+// constructor is called when an object of a class is created.
+
+public class Construct{
     int x;
     public Construct(){
-        x=5;
+        x = 5;
     }
     public static void main(String[] args){
         Construct myobj = new Construct();
