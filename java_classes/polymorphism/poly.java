3668febfbdfae08564c608a358dc01c10ffaf56c
Kabir202005 <Kabir202005@gmail.com>
2026-04-04 23:04:37 +0530

    Initial commit

---
+++ java_classes/polymorphism/poly.java
@@ -0,0 +1,28 @@
+public class poly {
+    public void animalsound(){
+        System.out.println("The animal makes a sound: ");
+    }
+}
+class pig extends poly{
+    public void animalsound(){
+        System.out.println("The pig says: wee wee");
+    }
+}
+class dog extends poly{
+    public void animalsound(){
+        System.out.println("The dog says: bow bow");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        poly animal = new poly();
+        poly mypig = new pig();    
+        poly mydog = new dog();
+
+        animal.animalsound();
+        mypig.animalsound();
+        mydog.animalsound();
+    }
+}
+    
+
